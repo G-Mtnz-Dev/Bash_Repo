eec88c286cb9a56f23b331ad9dcf106c07f6a14b
G-Mtnz Dev <dev.gmartinez@gmail.com>
2025-06-13 17:53:10 +0200

    +Syncro

---
+++ lap_linux_code/syncro_scripts/Batch/3_LnxRepo_Esp_Db_Docs-Elimin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#/home/gerar_kde/.Syncro/Batch/
+# Script para ejecutar FreeFileSync usando el archivo de configuración .ffs_batch
+
+/usr/local/bin/FreeFileSync /home/gerar_kde/.Syncro/Batch/3_LnxRepo_Esp_Db_Docs-Elimin.ffs_batch >> /home/gerar_kde/.logs/Syncro_Logs/sync_iconos.log 2>&1
---
+++ lap_linux_code/syncro_scripts/Batch/6_LLinux-Code_Esp_llinux-Elimin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#/home/gerar_kde/.Syncro/Batch/
+# Script para ejecutar FreeFileSync usando el archivo de configuración .ffs_batch
+
+/usr/local/bin/FreeFileSync /home/gerar_kde/.Syncro/Batch/6_LLinux-Code_Esp_llinux-Elimin.ffs_batch >> /home/gerar_kde/.logs/Syncro_Logs/sync_llinux_code.log 2>&1
